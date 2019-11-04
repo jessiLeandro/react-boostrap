e030705324a0ab2b2d564ac8cf71d71ed7a1453e
Jessi Leandro Castro <JLC@localhost.localdomain>
2019-11-04 10:49:01 -0300

    add notification in navBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from "react";
 import "./App.css";
 
+const notifyMe = () => {
+  // Verifica se o browser suporta notificações
+  if (!("Notification" in window)) {
+    alert("Este browser não suporta notificações de Desktop");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    new Notification("Hi there!");
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission(function(permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        new Notification("Hi there!");
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them any more.
+};
+
 function App() {
   return (
     <>
+      {/* <button onClick={() => notifyMe()}>Notifique me!</button> */}
       <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-gradient-primary">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="#" onClick={() => notifyMe()}>
           Navbar
         </a>
         <button
